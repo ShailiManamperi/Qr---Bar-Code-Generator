b5515d3a36deb6d9138d4356f920f13c21a3a57f
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-12-06 19:42:55 +0530

    finish generate barcode funtion

--- code_generator/src/main/java/lk/ijse/gdse/generator/Controller/GenerateCodeFormController.java
+++ code_generator/src/main/java/lk/ijse/gdse/generator/Controller/GenerateCodeFormController.java
@@ -3,17 +3,28 @@ package lk.ijse.gdse.generator.Controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextArea;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import lk.ijse.gdse.generator.util.Navigation;
 import lk.ijse.gdse.generator.util.Routes;
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.BarcodeImageHandler;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class GenerateCodeFormController  {
+public class GenerateCodeFormController   {
 
     @FXML
     private JFXButton btngenerate;
@@ -23,6 +34,12 @@ public class GenerateCodeFormController  {
 
     @FXML
     private ImageView imgchange;
+
+    @FXML
+    private AnchorPane imgCodePane;
+
+    @FXML
+    private AnchorPane imgpane;
 
     @FXML
     private Pane inPane;
@@ -37,24 +54,160 @@ public class GenerateCodeFormController  {
     private JFXRadioButton rbQrCode;
 
     @FXML
-    private JFXTextArea txtInput;
+    private JFXRadioButton rbDouble;
+
+    @FXML
+    private JFXRadioButton rbSingle;
+
+//    @FXML
+//    private JFXTextArea txtInput;
+
+    @FXML
+    private TextField txtinput1;
+
+    @FXML
+    private TextField txtinput2;
 
     @FXML
     private JFXButton btnExit;
 
-    @FXML
-    public void BarcodeOnAction(ActionEvent event) {
+    private String type;
 
+    private String flag;
+    Image fxImageBarCode;
+
+    private Image generatedCodeImage;
+
+    ImageView imageView;
+    public void initialize(){
+        txtinput1.setDisable(true);
+        txtinput2.setDisable(true);
+    }
+
+
+    @FXML
+    void CheckDoubleOnAction(ActionEvent event) {
+        flag = "Double";
+        txtinput1.setDisable(false);
+        txtinput2.setDisable(false);
     }
 
     @FXML
-    public void QrcodeOnAction(ActionEvent event) {
-
+    void CheckSingleOnAction(ActionEvent event) {
+        flag = "Single";
+        txtinput1.setDisable(false);
+        txtinput2.setDisable(true);
     }
 
     @FXML
     public void generateCodeOnAction(ActionEvent event) {
+        if (rbBarCode.isSelected()) {
+            type = "Bar Code";
+            generateBarCode();
+        } else if (rbQrCode.isSelected()) {
+            type = "Qr Code";
+            generateQRCode();
+        }
+        updatePreview();
+        btngenerate.setDisable(false);
+    }
 
+    private void generateBarCode() {
+        String input1 = txtinput1.getText();
+        String input2 = txtinput2.getText();
+        System.out.println(flag);
+        if (flag.equals("Single")){
+            if (input1 != null){
+                try {
+                    Barcode barcode = BarcodeFactory.createCode128(input1);
+                    barcode.setDrawingText(false);
+                    BufferedImage image = BarcodeImageHandler.getImage(barcode);
+
+                    // Resize the image
+                    int targetWidth = 180; // Set your desired width
+                    int targetHeight = 70; // Set your desired height
+
+                    // Convert the BufferedImage to JavaFX Image
+                    fxImageBarCode = SwingFXUtils.toFXImage(image, null);
+
+                    // Create an ImageView to scale the image
+                    imageView = new ImageView(fxImageBarCode);
+                    imageView.setFitWidth(targetWidth);
+                    imageView.setFitHeight(targetHeight);
+
+                    imgchange.setVisible(false);
+                    imageView.setVisible(true);
+
+                    // Set the alignment of the ImageView within the AnchorPane
+                    AnchorPane.setTopAnchor(imageView, (imgCodePane.getHeight() - imageView.getFitHeight()) / 2);
+                    AnchorPane.setLeftAnchor(imageView, (imgCodePane.getWidth() - imageView.getFitWidth()) / 2);
+
+                    imgCodePane.getChildren().add(imageView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // Handle any exceptions that may occur during barcode generation
+                }
+            }else {
+                throw new RuntimeException("\n Input filed is empty");
+            }
+        }else if (flag.equals("Double")){
+            if (input1 != null && input2 != null){
+                try {
+                    // First Barcode
+                    Barcode barcode1 = BarcodeFactory.createCode128(input1);
+                    barcode1.setDrawingText(false);
+                    BufferedImage image1 = BarcodeImageHandler.getImage(barcode1);
+                    // Resize the image
+                    int targetWidth = 100; // Set your desired width
+                    int targetHeight = 100; // Set your desired height
+
+                    // Second Barcode
+                    Barcode barcode2 = BarcodeFactory.createCode128(input2);
+                    barcode2.setDrawingText(false);
+                    BufferedImage image2 = BarcodeImageHandler.getImage(barcode2);
+
+                    // Combine the two images (You may want to adjust this based on your requirements)
+                    BufferedImage combinedImage = new BufferedImage(targetWidth * 2, targetHeight, BufferedImage.TYPE_INT_ARGB);
+                    Graphics2D g2d = combinedImage.createGraphics();
+                    g2d.drawImage(image1, 0, 0, null);
+                    g2d.drawImage(image2, targetWidth, 0, null);
+                    g2d.dispose();
+
+                    // Convert the combined BufferedImage to JavaFX Image
+                    fxImageBarCode = SwingFXUtils.toFXImage(combinedImage, null);
+
+                    // Create an ImageView to scale the combined image
+                    imageView = new ImageView(fxImageBarCode);
+                    imageView.setFitWidth(targetWidth * 2); // Adjust the width based on your needs
+                    imageView.setFitHeight(targetHeight);
+
+                    imgchange.setVisible(false);
+                    imageView.setVisible(true);
+
+                    // Set the alignment of the ImageView within the AnchorPane
+                    AnchorPane.setTopAnchor(imageView, (imgCodePane.getHeight() - imageView.getFitHeight()) / 2);
+                    AnchorPane.setLeftAnchor(imageView, (imgCodePane.getWidth() - imageView.getFitWidth()) / 2);
+
+                    imgCodePane.getChildren().add(imageView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    // Handle any exceptions that may occur during barcode generation
+                }
+            }else {
+                throw new RuntimeException("\n input filed are empty");
+            }
+        }
+
+
+    }
+
+    private void generateQRCode() {
+    }
+
+    private void updatePreview() {
+        if (generatedCodeImage != null) {
+            imgchange.setImage(generatedCodeImage);
+        }
     }
 
     @FXML
